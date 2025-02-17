039ac3e9f42769b7699d63ca0d27874ae30783a7
Diego <bastosdiegol@gmail.com>
2025-02-17 17:46:37 -0500

    chore: add firebase config and necessary dependencies

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,20 @@
+const sw = new URL("service-worker.js", import.meta.url);
+if ("serviceWorker" in navigator) {
+  const s = navigator.serviceWorker;
+  s.register(sw.href, {
+    scope: "/codex/",
+  })
+    .then((_) =>
+      console.log(
+        "Service Worker Registered for scope:",
+        sw.href,
+        "with",
+        import.meta.url
+      )
+    )
+    .catch((err) => console.error("Service Worker Error:", err));
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const bookList = document.getElementById("book-list");
   const bookForm = document.getElementById("book-form");
