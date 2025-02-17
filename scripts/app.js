ecb1e4a35593dfff6b68457721b43a763b807805
Diego <bastosdiegol@gmail.com>
2025-02-17 15:56:10 -0500

    feat: implement CRUD operations in Codex UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const bookList = document.getElementById("book-list");
+  const bookForm = document.getElementById("book-form");
   const menu = document.getElementById("side-menu");
   const overlay = document.getElementById("menu-overlay");
   const burgerMenu = document.getElementById("burger-menu");
   const closeButton = document.getElementById("close-menu");
+  const addBookLink = document.getElementById("add-book-link");
+  const saveBook = document.getElementById("save-book");
+  const closeBookForm = document.getElementById("close-form");
+  const deleteBook = document.getElementById("delete-book");
+  const bookId = document.getElementById("book-id");
 
   // Burguer Menu Functionality
   function openMenu() {
@@ -24,7 +30,8 @@ document.addEventListener("DOMContentLoaded", () => {
   let books = [
     {
       id: 1,
-      cover: "",
+      cover:
+        "https://www.royalroadcdn.com/public/covers-full/21220-mother-of-learning.jpg?time=1637247458",
       title: "Mother of Learning",
       author: "nobody103, Domagoj Kurmaić",
       genre: "Fantasy, Magic, Time Travel",
@@ -94,11 +101,17 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   ];
 
+  /**
+   * Display Books
+   * Renders each book as Artciles inside Book-List Section.
+   * @returns {void}
+   */
   function displayBooks() {
     bookList.innerHTML = "";
     books.forEach((book) => {
       // Book Article
       const bookCard = document.createElement("article");
+      bookCard.id = book.id;
       bookCard.classList.add("book-card");
       bookCard.setAttribute("aria-labelledby", `book-title-${book.id}`);
       // Book image container
@@ -106,12 +119,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const bookImgDiv = document.createElement("div");
         bookImgDiv.classList.add("book-img");
         const bookImg = document.createElement("img");
-        bookImg.src =
-          book.cover === ""
-            ? "./images/book-cover-placeholder.jpg"
-            : book.cover;
-        bookImg.alt = "Book Cover";
+        bookImg.src = book.cover;
+        bookImg.alt = `${book.title} Book Cover`;
         bookImgDiv.appendChild(bookImg);
+        bookCard.appendChild(bookImgDiv);
       }
       // Book info container
       const bookInfoDiv = document.createElement("div");
@@ -140,8 +151,7 @@ document.addEventListener("DOMContentLoaded", () => {
       bookInfoDiv.appendChild(bookGenre);
       bookInfoDiv.appendChild(bookStatus);
       bookInfoDiv.appendChild(bookProgress);
-      // Append image and info to article
-      if (book.cover !== "") bookCard.appendChild(bookImgDiv);
+      // Append info to article
       bookCard.appendChild(bookInfoDiv);
       bookCard.appendChild(bookRatingSpan);
       // Append Article and
@@ -149,9 +159,127 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  /**
+   * Utility Function that creates a string of stars based on the rating value.
+   * @param {number} rating - Rating value from 1 to 5
+   * @returns {string} - String of stars
+   */
   function renderStars(rating) {
     return "&starf;".repeat(rating) + "&star;".repeat(5 - rating);
   }
 
+  // Book Form Functionality
+  bookList.addEventListener("click", (event) => {
+    const bookCard = event.target.closest(".book-card");
+    if (bookCard) {
+      const bookId = bookCard.id;
+      const book = books.find((b) => b.id == bookId);
+      openBookForm(book);
+    }
+  });
+
+  /**
+   * Open Book Form
+   * Populates form fields with book data when editing
+   * or clears form fields when adding a new book entry.
+   * @param {Object|null} book - Book object to populate form fields
+   * @returns {void}
+   */
+  function openBookForm(book = null) {
+    const formCover = document.getElementById("book-cover");
+    const formId = document.getElementById("book-id");
+    const formTitle = document.getElementById("book-title");
+    const formAuthor = document.getElementById("book-author");
+    const formGenre = document.getElementById("book-genre");
+    const formProgress = document.getElementById("book-progress");
+    const formStatus = document.getElementById("book-status");
+    const formRating = document.getElementById("book-rating");
+
+    if (book) {
+      formCover.value = book.cover;
+      formId.value = book.id;
+      formTitle.value = book.title;
+      formAuthor.value = book.author;
+      formGenre.value = book.genre;
+      formProgress.value = book.progress;
+      formStatus.value = book.status;
+      formRating.value = book.rating;
+      deleteBook.style.display = "inline-block";
+    } else {
+      formCover.value = "";
+      formId.value = "";
+      formTitle.value = "";
+      formAuthor.value = "";
+      formGenre.value = "";
+      formProgress.value = "";
+      formStatus.value = "Not Read";
+      formRating.value = "";
+      deleteBook.style.display = "none";
+    }
+
+    bookForm.classList.add("show");
+    bookList.classList.add("hide");
+  }
+
+  // Save Book Button Functionality
+  saveBook.addEventListener("click", () => {
+    const form = document.getElementById("book-management-form");
+    if (!form.checkValidity()) {
+      form.reportValidity();
+      return;
+    }
+
+    const bookId = document.getElementById("book-id").value;
+    if (bookId) {
+      const bookIndex = books.findIndex((book) => book.id == bookId);
+      if (bookIndex !== -1) {
+        books[bookIndex] = {
+          id: bookId,
+          cover: document.getElementById("book-cover").src,
+          title: document.getElementById("book-title").value,
+          author: document.getElementById("book-author").value,
+          genre: document.getElementById("book-genre").value,
+          progress: document.getElementById("book-progress").value,
+          status: document.getElementById("book-status").value,
+          rating: document.getElementById("book-rating").value,
+        };
+        displayBooks();
+        bookForm.classList.remove("show");
+        bookList.classList.remove("hide");
+      }
+    }
+  });
+
+  // Close Form Button Functionality
+  closeBookForm.addEventListener("click", () => {
+    bookForm.classList.remove("show");
+    bookList.classList.remove("hide");
+  });
+
+  // Delete Book Button Functionality
+  deleteBook.addEventListener("click", function () {
+    if (bookId.value === "") return;
+    if (
+      confirm(
+        `Are you sure you want to delete the book "${
+          document.getElementById("book-title").value
+        }" entry?`
+      )
+    ) {
+      books = books.filter((book) => book.id != bookId.value);
+      displayBooks();
+      bookForm.classList.remove("show");
+      bookList.classList.remove("hide");
+    }
+  });
+
+  // Add Book Link Functionality
+  addBookLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    openBookForm();
+    closeMenu();
+  });
+
+  // First Display of Books on Page Load
   displayBooks();
 });
